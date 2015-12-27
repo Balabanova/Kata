c9d7eecf1d4e16918d1547dcf656ab8e11b40121
flash2048@gmail.com <flash2048@gmail.com>
2015-12-27 11:29:13 +0300

    Kata 1: palindrome

---
+++ Kata1_palindrome/Kata1_palindrome/PalindromeExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata1_palindrome
+{
+    public static class PalindromeExtension
+    {
+        public static bool IsPalindrome(this string str)
+        {
+            if(str == null) throw new ArgumentNullException("value");
+            str = string.Join("", str.Where(char.IsLetterOrDigit).Select(char.ToLower));
+            for (var i = 0; i < str.Length/2; i++)
+            {
+                if (char.ToLower(str[i]) != char.ToLower(str[str.Length - i - 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Kata1_palindrome/Kata1_palindrome/PalindromeTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata1_palindrome
+{
+    [TestClass]
+    public class PalindromeTests
+    {
+        [TestMethod]
+        public void StringIsNull()
+        {
+            string str = null;
+            try
+            {
+                var result = str.IsPalindrome();
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.AreEqual("value", e.ParamName);
+            }
+           
+        }
+
+        [TestMethod]
+        public void StringIsEmpty()
+        {
+            var str = "";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void StringContainsOneLetter()
+        {
+            var str = "a";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NotPalindrome()
+        {
+            var str = "abcdefg";
+            var result = str.IsPalindrome();
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NotSimplePalindrome()
+        {
+            var str = "ab";
+            var result = str.IsPalindrome();
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsShortPalindrome()
+        {
+            var str = "aba";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsLongPalindrome()
+        {
+            var str = "levellevellevel";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindromeWithRegister()
+        {
+            var str = "Shahs";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindromeWithPunctuation()
+        {
+            var str = "Was it a car or a cat I saw?";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindromeWithPunctuationRussianAndHard()
+        {
+            var str = "А бензин - о! - течет он из неба.";
+            var result = str.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+    }
+}
