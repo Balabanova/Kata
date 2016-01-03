879b0ec14c18b9cab2d8594c4049f7308906cec2
flash2048@gmail.com <flash2048@gmail.com>
2016-01-03 18:48:17 +0300

    Kata 2 - Tic Tac Toe

---
+++ Kata2_TicTacToe/Kata2_TicTacToe/TicTacToe.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Kata2_TicTacToe
+{
+    enum GameStatus
+    {
+        NobodyWins,
+        WinX,
+        WinZero,
+        InvalidPlayer,
+        InvalidPosition,
+        FieldIsUsed
+    }
+
+    public static class CharExtension
+    {
+        public static bool IsNotEmpty(this char self)
+        {
+            return self != '\0';
+        }
+    }
+
+    class TicTacToe
+    {
+        private int _size = 3;
+        private char[][] _fields;
+        private GameStatus? _winner;
+        public TicTacToe()
+        {
+            _fields = new char[_size][];
+            for (int i = 0; i < _size; i++)
+            {
+                _fields[i] = new char[_size];
+            }
+        }
+
+        private GameStatus SetPosition(int i, int j, char player)
+        {
+            if (_fields[i][j].IsNotEmpty())
+            {
+                return GameStatus.FieldIsUsed;
+            }
+            _fields[i][j] = player;
+            var gameStatus = Status();
+            if (gameStatus == GameStatus.WinZero || gameStatus == GameStatus.WinX)
+            {
+                _winner = gameStatus;
+            }
+            return gameStatus;
+        }
+
+        private GameStatus Status()
+        {
+            var diagonal1Count = 0;
+            var diagonal2Count = 0;
+
+            var diagonal1Winner = _fields[0][0];
+            var diagonal2Winner = _fields[_size - 1][0];
+            for (int i = 0; i < _size; i++)
+            {
+                var horizontalCount = 0;
+                var verticalCount = 0;
+
+                var horizontalWinner = _fields[i][0];
+                var verticalWinner = _fields[0][i];
+
+                for (int j = 0; j < _size; j++)
+                {
+                    if (horizontalWinner.IsNotEmpty() && horizontalWinner == _fields[i][j])
+                    {
+                        horizontalCount++;
+                    }
+                    if (verticalWinner.IsNotEmpty() && verticalWinner == _fields[j][i])
+                    {
+                        verticalCount++;
+                    }
+                }
+                if (horizontalCount == _size)
+                {
+                    return horizontalWinner == 'X' ? GameStatus.WinX : GameStatus.WinZero;
+                }
+                if (verticalCount == _size)
+                {
+                    return verticalWinner == 'X' ? GameStatus.WinX : GameStatus.WinZero;
+                }
+
+                if (diagonal1Winner.IsNotEmpty() && diagonal1Winner == _fields[i][i])
+                {
+                    diagonal1Count++;
+                }
+                if (diagonal2Winner.IsNotEmpty() && diagonal2Winner == _fields[_size - 1 - i][i])
+                {
+                    diagonal2Count++;
+                }
+
+            }
+
+            if (diagonal1Count == _size)
+            {
+                return diagonal1Winner == 'X' ? GameStatus.WinX : GameStatus.WinZero;
+            }
+
+            if (diagonal2Count == _size)
+            {
+                return diagonal2Winner == 'X' ? GameStatus.WinX : GameStatus.WinZero;
+            }
+
+            return GameStatus.NobodyWins;
+        }
+
+        public GameStatus Play(int i, int j, char player)
+        {
+            if (player != 'X' && player != '0')
+            {
+                return GameStatus.InvalidPlayer;
+            }
+            if (i < 1 || i > _size || j < 1 || j > _size)
+            {
+                return GameStatus.InvalidPosition;
+            }
+            if (_winner != null)
+            {
+                return _winner.Value;
+            }
+            return SetPosition(--i, --j, player);
+        }
+    }
+}
---
+++ Kata2_TicTacToe/Kata2_TicTacToe/TicTacToeTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata2_TicTacToe
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var game = new TicTacToe();
+            var result = game.Play(1, 1, 'X');
+            Assert.AreEqual(result, GameStatus.NobodyWins);
+        }
+
+        [TestMethod]
+        public void InvalidPlayer()
+        {
+            var game = new TicTacToe();
+            var result = game.Play(1, 1, 'z');
+            Assert.AreEqual(result, GameStatus.InvalidPlayer);
+        }
+
+        [TestMethod]
+        public void InvalidPosition()
+        {
+            var game = new TicTacToe();
+            var result = game.Play(4, 1, 'X');
+            Assert.AreEqual(result, GameStatus.InvalidPosition);
+        }
+
+        [TestMethod]
+        public void FieldIsUsed()
+        {
+            var game = new TicTacToe();
+            game.Play(1, 1, 'X');
+            var result = game.Play(1, 1, '0');
+            Assert.AreEqual(result, GameStatus.FieldIsUsed);
+        }
+
+        [TestMethod]
+        public void FirstLineZeroWins()
+        {
+            //000
+            //...
+            //...
+            var game = new TicTacToe();
+            game.Play(1, 1, '0');
+            game.Play(1, 2, '0');
+            var result = game.Play(1, 3, '0');
+            Assert.AreEqual(result, GameStatus.WinZero);
+        }
+
+        [TestMethod]
+        public void FirstColumnXWins()
+        {
+            //X..
+            //X..
+            //X..
+            var game = new TicTacToe();
+            game.Play(1, 1, 'X');
+            game.Play(2, 1, 'X');
+            var result = game.Play(3, 1, 'X');
+            Assert.AreEqual(result, GameStatus.WinX);
+        }
+
+        [TestMethod]
+        public void Diagonal1ZeroWins()
+        {
+            //0..
+            //.0.
+            //..0
+            var game = new TicTacToe();
+            game.Play(1, 1, '0');
+            game.Play(2, 2, '0');
+            var result = game.Play(3, 3, '0');
+            Assert.AreEqual(result, GameStatus.WinZero);
+        }
+
+        [TestMethod]
+        public void Diagonal2XWins()
+        {
+            //..X
+            //.X.
+            //X..
+            var game = new TicTacToe();
+            game.Play(3, 1, 'X');
+            game.Play(2, 2, 'X');
+            var result = game.Play(1, 3, 'X');
+            Assert.AreEqual(result, GameStatus.WinX);
+        }
+
+        [TestMethod]
+        public void OnlyFirstWinnerX()
+        {
+            //000
+            //XXX
+            //...
+            var game = new TicTacToe();
+            game.Play(2, 1, 'X');
+            game.Play(2, 2, 'X');
+            game.Play(2, 3, 'X');
+            game.Play(1, 1, '0');
+            game.Play(1, 2, '0');
+            var result = game.Play(1, 3, '0');
+            Assert.AreEqual(result, GameStatus.WinX);
+        }
+
+    }
+
+
+}
