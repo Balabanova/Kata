d0ea62705a8f1ffff88af8f2bc9a75ac3b247110
flash2048@gmail.com <flash2048@gmail.com>
2016-02-24 19:56:56 +0300

    Kata 3 - FizzBuzz

---
+++ Kata3_FizzBuzz/Kata3_FizzBuzz/FizzBuzzExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Kata3_FizzBuzz
+{
+    public static class FizzBuzzExtension
+    {
+        public static string FizzBuzz(this int self)
+        {
+            var result = new StringBuilder();
+            if (self%3 == 0)
+            {
+                result.Append("Fizz");
+            }
+
+            if (self%5 == 0)
+            {
+                result.Append("Buzz");
+            }
+
+            if (result.Length == 0)
+            {
+                result.Append(self.ToString());
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Kata3_FizzBuzz/Kata3_FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata3_FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void TestNumber_1()
+        {
+            var result = 1.FizzBuzz();
+            Assert.AreEqual(result,"1");
+        }
+        [TestMethod]
+        public void TestNumber_2()
+        {
+            var result = 2.FizzBuzz();
+            Assert.AreEqual(result, "2");
+        }
+        [TestMethod]
+        public void TestNumber_3()
+        {
+            var result = 3.FizzBuzz();
+            Assert.AreEqual(result, "Fizz");
+        }
+        [TestMethod]
+        public void TestNumber_5()
+        {
+            var result = 5.FizzBuzz();
+            Assert.AreEqual(result, "Buzz");
+        }
+        [TestMethod]
+        public void TestNumber_15()
+        {
+            var result = 15.FizzBuzz();
+            Assert.AreEqual(result, "FizzBuzz");
+        }
+    }
+}
